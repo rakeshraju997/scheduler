04bf71e0bbfdbe05509a2b0ac4e6b4138ee2c98c
Rakesh <rakesh.mentormerlin@gmail.com>
2021-07-15 17:37:23 +0530

    error corrected

--- lib/views/steps/_confirmation.php
+++ lib/views/steps/_confirmation.php
@@ -12,7 +12,8 @@
 		  <h5 class="confirmation-section-heading"><?php _e('Appointment Info', 'latepoint'); ?></h5>
 		  <ul>
 		  	<li><?php _e('Date:', 'latepoint'); ?> <strong><?php echo $booking->format_start_date_and_time(get_option('date_format'), false, OsTimeHelper::get_timezone_from_session());
-        if($booking->service->id == 3){echo ' - '.$booking->format_end_date_and_time(get_option('date_format'), false, OsTimeHelper::get_timezone_from_session());//end date only for three day session} ?></strong></li>
+        if($booking->service->id == 3){echo ' - '.$booking->format_end_date_and_time(get_option('date_format'), false, OsTimeHelper::get_timezone_from_session());//end date only for three day session
+        } ?></strong></li>
 		  	<li>
           <?php _e('Time:', 'latepoint'); ?> 
           <strong>
